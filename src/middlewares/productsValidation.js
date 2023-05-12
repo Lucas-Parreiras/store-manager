384f526eef9aa3ff1e40a5f7b83f9d7363e73e42
Lucas Parreiras <lucassouzaparreiras@hotmail.com>
2023-05-12 01:28:38 -0300

    Requisito 04 resolvido, implementada validação de inserção de produto.

---
+++ src/middlewares/productsValidation.js
@@ -0,0 +1,21 @@
+const BAD_REQUEST = 400;
+const UNPROCESSABLE_ENTITY = 422;
+
+function validateName(req, res, next) {
+    const { name } = req.body;
+
+    if (!name) {
+        return res.status(BAD_REQUEST).json({ message: '"name" is required' });
+    }
+
+    if (name.length < 5) {
+        return res.status(UNPROCESSABLE_ENTITY)
+            .json({ message: '"name" length must be at least 5 characters long' });
+    }
+
+    return next();
+}
+
+module.exports = {
+    validateName,
+};
--- src/routers/product.router.js
+++ src/routers/product.router.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const { productController } = require('../controllers');
 
+const { validateName } = require('../middlewares/productsValidation');
+
 const router = express.Router();
 
-router.post('/', productController.addNewProduct);
+router.post('/', validateName, productController.addNewProduct);
 
 router.get('/:id', productController.productById);
 
